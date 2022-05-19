725ba124f2f95e9d9ef3f46f4c6bbaad39761dbc
Jochem de Wit <s1133680@student.hsleiden.nl>
2022-05-19 16:39:28 +0200

    Feat: Started with ChooseTrainCards

---
+++ src/main/java/controllers/TrainCardDeckController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class TrainCardDeckController {
+    public void SelectCards(int amount, String color) {
+//        Open SelectCards view with form
+
+//        Check which color is needed
+        if (color.equals("grey")) {
+
+        } else {
+
+        }
+
+    }
+}
---
+++ src/main/java/views/SelectTrainCardView.java
@@ -0,0 +1,13 @@
+package views;
+
+import javafx.scene.input.MouseEvent;
+
+public class SelectTrainCardView {
+    public void confirmSelectedCards(MouseEvent mouseEvent) {
+//        SelectTrainCardView word gesloten
+
+//        Kaarten worden naar de Deposit stapel gegooid
+
+//        BuildTrain functie word aangeroepen
+    }
+}
