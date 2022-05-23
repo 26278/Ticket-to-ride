f4a44d80f75ac94c9cfb38a1eee3303f8a9c6e77
SRvanBeek <0705stijnvb@gmail.com>
2022-05-23 15:44:22 +0200

    feat: added color constants

---
+++ src/main/java/ttr/Constants/ColorConstants.java
@@ -0,0 +1,11 @@
+package ttr.Constants;
+
+public class ColorConstants {
+    static final String COLOR_RED = "red";
+    static final String COLOR_BLUE = "blue";
+    static final String COLOR_GREEN = "green";
+    static final String COLOR_YELLOW = "yellow";
+    static final String COLOR_WHITE = "white";
+    static final String COLOR_BLACK = "black";
+    static final String COLOR_RAINBOW = "rainbow";
+}
