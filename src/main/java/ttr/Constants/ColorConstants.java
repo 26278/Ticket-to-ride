f3c3c011347ae2fabeabf48284ab6ed11faccdd4
blaFish3 <30380030+blaFish3@users.noreply.github.com>
2022-05-30 12:34:48 +0200

    fix: made ColorConstants public

--- src/main/java/ttr/Constants/ColorConstants.java
+++ src/main/java/ttr/Constants/ColorConstants.java
@@ -1,11 +1,11 @@
 package ttr.Constants;
 
 public class ColorConstants {
-    static final String COLOR_RED = "red";
-    static final String COLOR_BLUE = "blue";
-    static final String COLOR_GREEN = "green";
-    static final String COLOR_YELLOW = "yellow";
-    static final String COLOR_WHITE = "white";
-    static final String COLOR_BLACK = "black";
-    static final String COLOR_RAINBOW = "rainbow";
+    public static final String COLOR_RED = "red";
+    public static final String COLOR_BLUE = "blue";
+    public static final String COLOR_GREEN = "green";
+    public static final String COLOR_YELLOW = "yellow";
+    public static final String COLOR_WHITE = "white";
+    public static final String COLOR_BLACK = "black";
+    public static final String COLOR_RAINBOW = "rainbow";
 }
