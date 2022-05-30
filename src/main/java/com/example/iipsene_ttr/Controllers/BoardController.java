8a64622cdebbabedf157a8ece501bd32486e3365
Robin Holswilder <s1121761@student.hsleiden.nl>
2022-05-30 10:09:53 +0200

    feat: you can hover over the boxes and then highlights the chosen boxes

--- src/main/java/com/example/iipsene_ttr/Controllers/BoardController.java
+++ src/main/java/com/example/iipsene_ttr/Controllers/BoardController.java
@@ -1,21 +1,53 @@
 package com.example.iipsene_ttr.Controllers;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseDragEvent;
+import javafx.scene.Node;
+import javafx.scene.effect.Glow;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
 
 public class BoardController {
-    @FXML
-    public void highlight(){
-        System.out.println("highlight");
+
+    public Rectangle Edinburgh_London_R1;
+    public Rectangle Edinburgh_London_R2;
+    public Rectangle Edinburgh_London_R3;
+    public Rectangle Edinburgh_London_R4;
+    public ArrayList<Rectangle> route = new ArrayList<>();
+    public javafx.scene.layout.AnchorPane AnchorPane;
 
 
-    }
     @FXML
-    public void no_highlight(){
-        System.out.println("no_highlight");
+    public void highlight() {
+        // functie om het geselecteerde vakje te highlighten
+        route.add(Edinburgh_London_R1);
+        route.add(Edinburgh_London_R2);
+        route.add(Edinburgh_London_R3);
+        route.add(Edinburgh_London_R4);
+        Glow glow = new Glow();
+        glow.setLevel(1);
+        for (int i = 0; i < route.size(); i++) {
+                route.get(i).setEffect(glow);
+        }
     }
+
     @FXML
-    public void place_train_or_station(){
+    public void no_highlight() {
+        Glow glow = new Glow();
+        glow.setLevel(0);
+        for (int i = 0; i < route.size(); i++) {
+            route.get(i).setEffect(glow);
+
+        }
+
+    }
+
+    @FXML
+    public void place_train_or_station() {
+        // functie om een trein of station te bouwen
+
         System.out.println("place");
     }
 }
