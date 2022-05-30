1616926f2b6126163a3e112daf215a5eb876e2a4
Jochem de Wit <s1133680@student.hsleiden.nl>
2022-05-30 14:46:17 +0200

    fix: rebase

--- src/main/java/ttr/Controllers/TrainCardDeckController.java
+++ src/main/java/ttr/Controllers/TrainCardDeckController.java
@@ -39,22 +39,6 @@ public class TrainCardDeckController {
         return trainCardCheckboxes;
     }
 
-
-import javafx.fxml.FXML;
-
-public class TrainCardDeckController {
-
-    public void SelectCards(int amount, String color) {
-//        Open SelectCards view with form
-
-//        Check which color is needed
-        if (color.equals("grey")) {
-
-        } else {
-
-        }
-    }
-
     @FXML
     protected void confirmSelectedCards() {
 
