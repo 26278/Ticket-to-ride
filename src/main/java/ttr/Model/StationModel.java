329061c65fd0c901803a8e7c82d9cc9ca817c027
Kwytze <s1137423@student.hsleiden.nl>
2022-06-12 15:20:39 +0200

    boardView, StationModel gemurged en opslaan

---
+++ src/main/java/ttr/Model/StationModel.java
@@ -0,0 +1,56 @@
+package ttr.Model;
+
+import ttr.Views.StationObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StationModel implements StationObserver{
+    private List<StationObserver> observers = new ArrayList<StationObserver>();
+
+    private String color;
+    private String groupName;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void placeStation(String rectangleGroupName, String stationColor){
+        // eerst firebase hier stop ik trein;
+        this.color = stationColor;
+        this.groupName = rectangleGroupName;
+
+        // daarna een update naar de view;
+        this.notifyObservers();
+
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (StationObserver observer: this.observers){
+            observer.update(this);
+        }
+    }
+
+    @Override
+    public void addObserver(StationObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(StationObserver observer) {
+        observers.remove(observer);
+    }
+
+
+    @Override
+    public void update(StationModel stationModel) {
+
+    }
+}
+
+
