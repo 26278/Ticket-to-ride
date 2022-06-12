c59f30307498bf13a3bb70f5a78c55c3a790fd67
s1135796 <s1135796@student.hsleiden.nl>
2022-06-12 21:17:40 +0200

    chore: changed when something happens when pulling cards

--- src/main/java/ttr/Model/TrainCardDeckModel.java
+++ src/main/java/ttr/Model/TrainCardDeckModel.java
@@ -68,9 +68,9 @@ public class TrainCardDeckModel implements Observable {
             discardTrainDeck.add(trainCardDeck.get(1));
         } else {
             //decreases amount of cards of one color
-            firestoreService.updateField(TRAINCARD_DECK, returnHand.get(0).getCardColor(),
+            firestoreService.updateField(TRAINCARD_DECK, trainCardDeck.get(0).getCardColor(),
                     String.valueOf(firestoreService.getTrainCardValue(String.valueOf(trainCardDeck.get(0))) - 1));
-            firestoreService.updateField(TRAINCARD_DECK, returnHand.get(1).getCardColor(),
+            firestoreService.updateField(TRAINCARD_DECK, trainCardDeck.get(1).getCardColor(),
                     String.valueOf(firestoreService.getTrainCardValue(String.valueOf(trainCardDeck.get(1))) - 1));
             returnHand.add(trainCardDeck.get(1));
             returnHand.add(trainCardDeck.get(0));
